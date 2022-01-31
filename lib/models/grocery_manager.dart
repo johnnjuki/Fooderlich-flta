704ead28158e0f83b359ed9e626d1e096af2cc49
John Njuki <njukijohn2000@gmail.com>
2022-01-31 23:34:00 +0300

    Update grocery manager

--- lib/models/grocery_manager.dart
+++ lib/models/grocery_manager.dart
@@ -3,11 +3,31 @@ import 'grocery_item.dart';
 
 class GroceryManager extends ChangeNotifier {
   final _groceryItems = <GroceryItem>[];
+  int _selectedIndex = -1;
+  bool _createNewItem = false;
 
   List<GroceryItem> get groceryItems => List.unmodifiable(_groceryItems);
 
+  int get selectedIndex => _selectedIndex;
+
+  GroceryItem? get selectedGroceryItem =>
+      _selectedIndex != -1 ? _groceryItems[_selectedIndex] : null;
+
+  bool get isCreatingNewItem => _createNewItem;
+
+  void createNewItem() {
+    _createNewItem = true;
+    notifyListeners();
+  }
+
   void deleteItem(int index) {
     _groceryItems.removeAt(index);
+    notifyListeners();
+  }
+
+  void groceryItemTapped(int index) {
+    _selectedIndex = index;
+    _createNewItem = false;
     notifyListeners();
   }
 
