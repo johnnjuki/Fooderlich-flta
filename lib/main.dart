68f22b04409c506b218f37f333591361103bc0f8
John Njuki <njukijohn2000@gmail.com>
2022-02-03 17:46:28 +0300

    Handle the Android system's back button

--- lib/main.dart
+++ lib/main.dart
@@ -63,6 +63,7 @@ class _FooderlichState extends State<Fooderlich> {
             title: 'Fooderlich',
             home: Router(
               routerDelegate: _appRouter,
+              backButtonDispatcher: RootBackButtonDispatcher(),
             ),
           );
         },
